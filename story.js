7a9b1684bec008a5a27857dc7fb24bc61fdbebe1
Gerardo Keys <jotojry@icloud.com>
2019-01-14 04:46:31 +0000

    first commit

---
+++ story.js
@@ -0,0 +1,8 @@
+/* 
++First program in JavaScript+
+Developed by: Gerardo Keys
+This will ask user for a noun (ppp), verb and adjective as well as store the value
+to a variable */
+var noun = prompt('Please provide a noun: ');
+var verb = prompt('Please proved a verb: ');
+var adjective = prompt('Please provide an adjective: ');
