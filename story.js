677fdc5da4622a04e8e5c1064f3f487e4d4217a5
Gerardo Keys <jotojry@icloud.com>
2019-01-14 06:01:42 +0000

    completed challenge

--- story.js
+++ story.js
@@ -3,6 +3,9 @@
 Developed by: Gerardo Keys
 This will ask user for a noun (ppp), verb and adjective as well as store the value
 to a variable */
+console.log('prompting user for input...');
 var noun = prompt('Please provide a noun: ');
 var verb = prompt('Please proved a verb: ');
 var adjective = prompt('Please provide an adjective: ');
+document.write('They knew him as the ' + noun + ', although they will never know how he  ' + verb + ' so ' + adjective);
+console.log('program end.');
